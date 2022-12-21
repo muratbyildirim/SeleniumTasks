a452dfb151088ffe06668a8456058cb57794e9d0
Murat Yıldırım <muratbyildirim@gmail.com>
2022-12-22 01:29:16 +0300

    Selenium_Day2

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
@@ -0,0 +1,65 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T5_getText_getAttribute {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go to: https://practice.cydeo.com/registration_form
+        driver.get("https://practice.cydeo.com/registration_form");
+
+        //3- Verify header text is as expected:
+        WebElement headerText=driver.findElement(By.tagName("h2"));
+        //Expected: Registration form
+        String expectedHeaderText="Registration form";
+        String actualHeaderText=headerText.getText();
+
+        if (actualHeaderText.equals(expectedHeaderText)){
+            System.out.println("Header text verification PASSED!");
+        }else{
+            System.out.println("Header text verification FAILED!!!");
+        }
+
+        //4- Locate “First name” input box
+        // we are locating the web element using "name" locator
+        // name attribute has "firstname" value
+        WebElement firsNameInput=driver.findElement(By.className("form-control"));
+
+        //5- Verify placeholder attribute’s value is as expected:
+        // Expected: first name
+        String expectedPlaceHolder="first name";
+        String actualPlaceHolder=firsNameInput.getAttribute("placeholder");
+
+        if (actualPlaceHolder.equals(expectedPlaceHolder)){
+            System.out.println("Placeholder text verification PASSED!");
+        }else{
+            System.out.println("Placeholder text verification FAILED!!!");
+        }
+
+        WebElement lastNameInput= driver.findElement(By.name("lastname"));
+
+        String expectedLastNamePlaceHolder="last name";
+        String actualLastNamePlaceHolder=lastNameInput.getAttribute("placeholder");
+        System.out.println(actualLastNamePlaceHolder);
+
+        if (actualLastNamePlaceHolder.equals(expectedLastNamePlaceHolder)){
+            System.out.println("Placeholder text verification PASSED!");
+        }else{
+            System.out.println("Placeholder text verification FAILED!!!");
+        }
+
+
+        driver.close();
+
+    }
+
+}
